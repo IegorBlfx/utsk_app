ca146e2a50fa2e4ef0c95f0025af44942086c1a9
Iegor <30709191e@gmail.com>
2019-02-04 15:44:23 +0200

    models. product

--- src/apps/account/models.py
+++ src/apps/account/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class TypeOfProduct(models.Model):
+    product_type = models.TextField(unique=True, null=False, blank=False)
+
+class Steel(models.Model):
+    name = models.TextField(blank=False, null=False)
+
+class Standard(models.Model):
+    name = models.TextField(blank=False, null=False)
+    number = models.TextField(blank=False, null=False)
+    comment = models.TextField(blank=True, null=True)
+    product_type = models.ForeignKey(TypeOfProduct, on_delete=models.SET_NULL(), related_name='standards')
+    steel = models.ManyToManyField(Steel, on_delete=models.SET_NULL(), related_name='standards')
+
+class MechanicalProperties(models.Model):
+    strength_yield = models.SmallIntegerField()
+    strength_tensile = models.SmallIntegerField()
+    extending = models.FloatField()
+    standard = models.ForeignKey(Standard, on_delete=models.SET_NULL(), related_name='properties')
+
+
+class Product(models.Model):
+    # In Product model shs means structural hollow sections, and rp means round pipe
+    width_shs = models.FloatField(null=False, blank=True)
+    height_shs = models.FloatField(null=False, blank=True)
+    diameter_pipe = models.FloatField(null=False, blank=True)
+    wall = models.FloatField(null=False, blank=False)
+    product_type = models.ForeignKey(TypeOfProduct, on_delete=models.SET_NULL(), related_name='products')
+
+
